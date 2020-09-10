67567a170a1a3ae5b54ccc7221f883f92b181f20
PrinceLee <prince805962788@163.com>
2020-09-10 22:56:01 +0800

    ts验证

--- examples/server.js
+++ examples/server.js
@@ -9,11 +9,34 @@ const app = express()
 const compiler = webpack(WebpackConfig)
 const router = express.Router()
 
-router.get('/simple/get', function(req, res) {
+router.get('/simple/get', function (req, res) {
   res.json({
     msg: `hello world`
   })
 })
+
+function registerBaseRouter() {
+  router.get('/base/get', function (req, res) {
+    res.json(req.query)
+  })
+
+  router.post('/base/post', function (req, res) {
+    res.json(req.body)
+  })
+
+  router.post('/base/buffer', function (req, res) {
+    let msg = []
+    req.on('data', (chunk) => {
+      if (chunk) {
+        msg.push(chunk)
+      }
+    })
+    req.on('end', () => {
+      let buf = Buffer.concat(msg)
+      res.json(buf.toJSON())
+    })
+  })
+}
 
 app.use(router)
 
@@ -30,7 +53,9 @@ app.use(webpackHotMiddleware(compiler))
 app.use(express.static(__dirname))
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 const port = process.env.PORT || 8088
 module.exports = app.listen(port, () => {
